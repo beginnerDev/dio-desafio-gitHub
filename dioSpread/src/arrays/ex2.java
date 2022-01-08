0d7ad166ecd96fe1967d2d50a330d0f92112c99b
Ruan thiago <ruanthiago.in@gmail.com>
2022-01-08 12:16:10 -0300

    resoluções, laços e arrays

---
+++ dioSpread/src/arrays/ex2.java
@@ -0,0 +1,22 @@
+package arrays;
+import java.util.Random;
+public class ex2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+        System.out.println("Números aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+        System.out.println("Sucessores dos números aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero + 1) + " ");
+        }
+    }
+}
